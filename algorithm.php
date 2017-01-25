7388f79f529c7c49c5fd30cf3f0d5fa361b1b47d
ocoeurr <slesarenko.anna@gmail.com>
2017-01-25 13:04:12 +0700

    Update algorithm.php

--- algorithm.php
+++ algorithm.php
@@ -9,18 +9,16 @@ $systemNumber2 = 1;
 do {
   if ($systemNumber1 > $userNumber) {
   echo "задуманное число НЕ входит в числовой ряд";
-  break;
 } else if ($systemNumber1 = $userNumber)
         {
           echo "задуманное число входит в числовой ряд";
-          break;
         }
         else {
           $systemNumber3 = $systemNumber1;
           $systemNumber1 = $systemNumber1 + $systemNumber2;
           $systemNumber2 = $systemNumber3;
         }
-      } while ($systemNumber1 <= $userNumber);
+      } while ($systemNumber1 < $userNumber);
       echo "</br>";
       echo "ваше число: ".$userNumber;
  ?>

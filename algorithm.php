f8aa798c0f2c91d0604970f97ded61239aeb631e
ocoeurr <slesarenko.anna@gmail.com>
2017-02-05 00:30:36 +0700

    Update algorithm.php

--- algorithm.php
+++ algorithm.php
@@ -6,19 +6,19 @@ ini_set('display_errors',1);
 $userNumber = rand(0,100);
 $systemNumber1 = 1;
 $systemNumber2 = 1;
-do {
-  if ($systemNumber1 > $userNumber) {
-  echo "задуманное число НЕ входит в числовой ряд";
-} else if ($systemNumber1 = $userNumber)
-        {
-          echo "задуманное число входит в числовой ряд";
+
+while ($systemNumber1 < $userNumber) {
+  $systemNumber3 = $systemNumber1;
+  $systemNumber1 = $systemNumber1 + $systemNumber2;
+  $systemNumber2 = $systemNumber3;
+}
+
+if ($systemNumber1 > $userNumber) {
+    echo "задуманное число НЕ входит в числовой ряд";
+      } else {
+        echo "задуманное число входит в числовой ряд";
         }
-        else {
-          $systemNumber3 = $systemNumber1;
-          $systemNumber1 = $systemNumber1 + $systemNumber2;
-          $systemNumber2 = $systemNumber3;
-        }
-      } while ($systemNumber1 < $userNumber);
-      echo "</br>";
-      echo "ваше число: ".$userNumber;
+
+echo "</br>";
+echo "ваше число: ".$userNumber;
  ?>
